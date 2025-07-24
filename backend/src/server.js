95f86edd8f15c300a611e756e44c91fecf035ef9
anshifood <anshiagarwal2905@gmail.com>
2025-07-25 00:14:58 +0530

    backend plus frontend integration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,54 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/',
+  
+  
+  (req, res) => {
+    res.send('<i>Server is ready </i>');
+}
+
+
+);
+
+// a list of 5 jokes 
+/*
+
+*/
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'Why did the scarecrow win an award? Because he was outstanding in his field!'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'Why don’t scientists trust atoms? Because they make up everything!'
+        },
+        {
+            id: 3,
+            title: 'A third joke',
+            content: 'I told my wife she was drawing her eyebrows too high. She looked surprised.'
+        },
+        {
+            id: 4,
+            title: 'A fourth joke',
+            content: 'What do you call a fake noodle? An Impasta!'
+        },
+        {
+            id: 5,
+            title: 'A fifth joke',
+            content: 'Why did the bicycle fall over? Because it was two tired!'
+        }
+    ];
+    res.send(jokes);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Serve at http://localhost:${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
   return (
-    <>
+    <div>
       <h1>Welcome to OurProject1</h1>
-      <p>
-        This is our project - By Aradhya n priyanshi.
-      </p>
-    </>
+      <p>JOKES: {jokes.length}</p>
+
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
